8e1cad1a3552b111ee9acb7c1251c762135296d1
Agarkovsergey <agarkovsergey91@mail.ru>
2021-01-05 01:59:18 +0200

    task4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,29 @@
 'use strict'
+const formatString = function(string) {
+  // твой код
+    if (string.length > 40) {
+        let shortString = string.slice(1, 40);
+        shortString += "...";
+        return shortString;
+    } 
+    return string;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+// вернется оригинальная строка
+
+console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+// вернется форматированная строка
+
+console.log(formatString('Curabitur ligula sapien.'));
+// вернется оригинальная строка
+
+console.log(
+  formatString(
+    'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',
+  ),
+);
+// вернется форматированная строка
