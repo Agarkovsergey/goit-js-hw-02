845e9b09766300429d07bc34b44921bfc423b9a8
Agarkovsergey <agarkovsergey91@mail.ru>
2021-01-05 01:32:00 +0200

    task3

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,21 @@
 'use strict'
+const findLongestWord = function(string) {
+  // твой код
+    let words = string.split(' ');
+    let longWord = words[0];
+    for (let i = 0; i < words.length; i += 1){
+        if (words[i].length > longWord.length) {
+            longWord = words[i];
+        }
+    }
+    return longWord;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+
+console.log(findLongestWord('Google do a roll')); // 'Google'
+
+console.log(findLongestWord('May the force be with you')); // 'force'
