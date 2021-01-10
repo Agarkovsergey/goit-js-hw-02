810274bd00bd90676918715d6eed66ba93b0affd
Agarkovsergey <agarkovsergey91@mail.ru>
2021-01-10 13:57:54 +0200

    task6

--- js/task-6.js
+++ js/task-6.js
@@ -1,1 +1,26 @@
 'use strict'
+let input;
+const numbers = [];
+let total = 0;
+
+while(true) {
+    input = prompt('введите число');
+    if (input === null) {
+        if (numbers.length !== 0) {
+            for (const number of numbers) {
+                total += number;
+            }
+            console.log(numbers);
+            console.log(total);
+        } else {
+            console.log('empty');
+        }
+        break;
+    }    
+    input = Number(input);
+    if (Number.isNaN(input)) {
+        alert(`Было введено не число, попробуйте еще раз`)
+        continue;
+    }
+    numbers.push(input)
+} 
